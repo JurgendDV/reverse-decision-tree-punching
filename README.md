0e3f46c82f5f63679bc9ea5ac4d1a922a6c38913
Jurgen De Bruyne <JurgenD@PAVERCO.be>
2019-06-07 13:45:28 +0200

    csv and python added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # reverse-decision-tree-punching
+Successfully installed joblib-0.13.2 scikit-learn-0.21.2 scipy-1.3.0
---
+++ reverse-decision-tree.py
@@ -0,0 +1,3 @@
+#resources: https://www.youtube.com/watch?v=fS70iptz-XU&list=PL6SXJNGTndC7gBrOoLGo45McH5tvUf8df&index=2&t=2571s
+import numpy as np
+
